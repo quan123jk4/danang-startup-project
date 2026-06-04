a9ddd37f2cd444e6145fade1e424fcbbcb18ad74
Quân <minhquan210205@gmail.com>
2026-06-04 10:49:41 +0700

    modify post

--- AI/main.py
+++ AI/main.py
@@ -73,4 +73,5 @@ async def generate_itinerary(data: GenerateRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
